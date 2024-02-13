d91cbd8fff981f5002870d7f37aec5ff6cf6e356
roel.edeleon <roel.edeleon@gmail.com>
2024-02-14 00:26:37 +0800

    Commit 20240214 0026H

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+
+import * as simpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items.js';
+// Additional styles import
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import simpleLightbox from 'simplelightbox';
+
 // Change code below this line
 
-console.log(galleryItems);
+// create gallery markup
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('gallery__item');
+
+  listItem.innerHTML = `
+    <a class="gallary__link" href="${item.original}">
+        <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+        />
+    </a>
+    `;
+
+  gallery.appendChild(listItem);
+});
+
+//Even delegationn to open modal
+gallery.addEventListener('click', eventx => {
+  eventx.preventDefault();
+
+  if (event.target.classList.contains('gallery__image')) {
+    const imageUrl = event.target.dataset.source;
+    const instance = simpleLightbox.create(
+      `<img src="${imageUrl}" alt="Image">`
+    );
+    instance.show();
+  }
+});
